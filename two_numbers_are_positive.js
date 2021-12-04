09079022d85e0664ba7f47004d2cc49df72b84a8
pypymi <dr.mizoguchi@gmail.com>
2021-12-04 10:54:39 +0900

    test

--- two_numbers_are_positive.js
+++ two_numbers_are_positive.js
@@ -2,6 +2,5 @@ function twoArePositive(a, b, c) {
   // Happy Coding
   let array = [a,b,c];
   let result1 = array.filter(v => v>0)
-  let result2 = array.filter(v => v<0)
-  return result1.length==2 && result2.length==1 ? true : false;
+  return result1.length==2 ? true : false;
 }
